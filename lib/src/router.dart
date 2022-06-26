180714b9be6c0b28081265e051726b5ce818ffa9
Mohammad <mohammad1@progiom.com>
2022-06-26 21:54:24 +0400

    adding router

---
+++ lib/src/router.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sport_app/src/features/fixtures/presentation/pages/fixtures_home_page.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return CupertinoPageRoute(builder: (_) => const FixturesHomePage());
+      default:
+        return CupertinoPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
