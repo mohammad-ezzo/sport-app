2c151dac7a440286349f33f6272dd5feec33ee23
Mohammad <mohammad1@progiom.com>
2022-06-26 21:55:07 +0400

    adding top level App Widget

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:sport_app/src/router.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      onGenerateRoute: AppRouter.generateRoute,
+    );
+  }
+}
